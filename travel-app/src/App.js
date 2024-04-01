e5e4127733490686309092ac2a5cffda38fdfd2c
Umayangi Ekanayake <umayangi1999@gmail.com>
2024-04-01 08:53:59 +0530

    updated the project

--- travel-app/src/App.js
+++ travel-app/src/App.js
@@ -1,5 +1,10 @@
-function App() {
-  return <div className="App"></div>;
+import React from "react";
+import './app.css'
+
+const App = () => {
+  return (
+    <h1>Working</h1>
+  );
 }
 
-export default App;
+export default App 
